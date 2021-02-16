14fd8a667fd58b258edaee8561a5b2d2802cfb22
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    cas: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- device.mk
+++ device.mk
@@ -29,7 +29,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -345,7 +347,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.xiaomi-libperfmgr
 
 # QMI
 PRODUCT_PACKAGES += \

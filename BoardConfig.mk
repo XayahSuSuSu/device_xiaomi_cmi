4711251634a66a644b5be0183829cb16054fee8a
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-06-23 13:03:46 +0800

    cas: Remove TARGET_USES_INTERACTION_BOOST flag
    * Doesn't exist in power-libperfmgr
    
    Change-Id: Ie4d60caf8b69757ef1dea109a5c146d6953faa74

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,7 +148,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop

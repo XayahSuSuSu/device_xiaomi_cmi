498f0aa3fb593b65548ec9cbd4a6bf7da508fb60
Xayah <zds1249475336@gmail.com>
2021-07-26 13:15:33 +0800

    cas: vibrator: Switch to QTI vibrator

--- device.mk
+++ device.mk
@@ -432,7 +432,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_kona
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
